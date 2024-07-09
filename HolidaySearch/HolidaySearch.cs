1d2553ae576b30bdbeb7df3b9bda38ac32a1e73b
WINDOWS-1JQV1IC\gavin <gavinhatton90@googlemail.com>
2024-07-09 11:42:19 +0100

    Added data files and updated test to be green by adding hotel lookup func

--- HolidaySearch/HolidaySearch.cs
+++ HolidaySearch/HolidaySearch.cs
@@ -1,11 +1,41 @@
 ﻿
+using System;
+using System.ComponentModel;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace HolidaySearch
 {
     public class HolidaySearch
     {
-        public object Search()
+        public IEnumerable<Hotel> Search()
         {
-            throw new NotImplementedException();
+            var hotels = GetHotels();
+
+            return hotels;
+        }
+
+        
+        public IEnumerable<Hotel> GetHotels()
+        {
+            var results = GetData<Hotel>("HotelData.json");
+            return results;
+        }
+
+        private IEnumerable<T> GetData<T>(string filename)
+        {
+            using (var reader = new StreamReader(GetFullFilePath(filename)))
+            {
+                return JsonSerializer.Deserialize<T[]>(reader.BaseStream)
+                    ?? Enumerable.Empty<T>();
+            }
+        }
+
+        private string GetFullFilePath(string filename)
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "Data", filename);
         }
     }
+
+
 }
---
+++ HolidaySearch/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch
+{
+    public class Hotel
+    {
+
+        public int Id { get; set; }
+
+        public IList<string> LocalAirports { get; set; }
+
+        public int Price { get; set; }
+
+        public int Nights { get; set; }
+
+        public DateTime ArrivalDate { get; set; }
+
+    }
+}
